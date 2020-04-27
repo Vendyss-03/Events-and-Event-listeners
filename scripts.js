745676b18ed1886a45cb5136cc6b746c899560d7
Vendyss-03 <64092106+Vendyss-03@users.noreply.github.com>
2020-04-27 17:53:07 +0200

    Added header & input, random color to change background of buttons

--- scripts.js
+++ scripts.js
@@ -7,7 +7,10 @@ $('#second-button').click((event) => {
 });
 
 $('#prettifier').click((event) => {
-    $('button').css('background', 'aquamarine');
+
+    let chooseColor = document.getElementById("randomcolor").value;
+
+    $('button').css('background', chooseColor);
     $('button').css('border', '2px solid #0b8ec1');
     $('button').css('border-radius', '50px');
     $('button').css('font-size', '20px');
